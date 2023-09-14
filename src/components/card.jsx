4ee86c58459b0591bb0ac020f73117e5d8791155
rhmunna143 <rhmunna19@gmail.com>
2023-09-14 22:47:10 +0600

    created cards using fake data

---
+++ src/components/card.jsx
@@ -0,0 +1,32 @@
+import { FiDollarSign } from 'react-icons/fi';
+import { TfiBook } from 'react-icons/tfi';
+
+
+
+const Card = ({ course }) => {
+    return (
+        <div className="p-4 rounded-md bg-white">
+            <div>
+                <img src={course.image} alt="" />
+            </div>
+
+            <dir className="text-left p-0">
+                <h4 className="font-semibold mb-2">{course.title}</h4>
+                <p className="mb-2">{course.description}</p>
+            </dir>
+
+            <div className='flex justify-between items-center'>
+                <FiDollarSign />
+                <p>Price : {course.price}</p>
+                <TfiBook />
+                <p>Credit : {course.credit}hr</p>
+            </div>
+
+            <div>
+                <button className='bg-[#2F80ED] text-white w-full p-2 mt-4 rounded-md font-medium'>Select</button>
+            </div>
+        </div>
+    )
+}
+
+export default Card;
--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+import Card from "./card"
+import { data } from "autoprefixer";
+
 const Home = () => {
+    const [cards, setCards] = useState([])
+
+    useEffect(() => {
+        fetch("../../public/data.json")
+            .then((res) => res.json())
+            .then((data) => setCards(data))
+            .catch((error) => console.error(error)); 
+    }, []);
+
+
     return (
-        <div>
-            
+        <div className="container mx-auto flex p-4 lg:p-0">
+            <div className="cards grid-cols-1 grid lg:grid-cols-3 md:grid-cols-2 gap-4">
+                {cards.map((course) => (
+                    <Card key={course.id} course ={course}></Card>
+                ))}
+            </div>
+
+            <div className="carts lg:w-[790px]">
+
+            </div>
         </div>
     )
 }
